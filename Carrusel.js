25186f015eb78b3567e723c3e08d4e96610a36b9
Oleoleo61 <u24251989@utp.edu.pe>
2026-06-15 18:23:53 -0500

    Mejora de fuentes

--- Carrusel.js
+++ Carrusel.js
@@ -1,21 +1,32 @@
-let index =0;
-const track= document.getElementById("carrusel");
-const img=track.children;
+document.addEventListener("DOMContentLoaded", () => {
+    let index = 0;
+    const track = document.getElementById("carrusel");
+    const img = track.children;
 
-document.getElementById("next").addEventListener("click",() =>{
-index++;
-if(index >= img.length) index=0;
-track.scrollLeft = img[index].offsetLeft;
+    let autoScroll = setInterval(moverSiguiente, 3500);
+
+    function moverSiguiente() {
+        index++;
+        if (index >= img.length) index = 0;
+        track.scrollLeft = img[index].offsetLeft;
+    }
+
+    function reiniciarAutoScroll() {
+    clearInterval(autoScroll);
+    autoScroll = setInterval(moverSiguiente, 3500);
+    }
+
+    document.getElementById("next").addEventListener("click", () => {
+        clearInterval(autoScroll);
+        moverSiguiente();
+        reiniciarAutoScroll();
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+        clearInterval(autoScroll);
+        index--;
+        if (index < 0) index = img.length - 1;
+        track.scrollLeft = img[index].offsetLeft;
+        reiniciarAutoScroll();
+    });
 });
-
-document.getElementById("prev").addEventListener("click",()=>{
-index--;
-if(index<0) index=img.length-1;
-track.scrollLeft = img[index].offsetLeft;
-});
-
-setInterval(() => {
-    index++;
-    track.scrollLeft=img[index].offsetLeft;
-    if(index>=img.length) index=0;
-},4500);
